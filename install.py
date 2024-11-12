31d26816c80c220010e7706322e1a7ed2ca7a4bf
Enviction <schmidtjonas662@gmail.com>
2024-11-12 15:05:07 +0100

    install update

--- install.py
+++ install.py
@@ -2,6 +2,6 @@ from dulwich import porcelain
 import sys
 
 if sys.argv[1] == "update":
-	porcelain.pull(".", remote_location="https://github.com/Enviction/LiberIPad.git")
+	porcelain.pull(".", remote_location="https://github.com/E3nviction/LiberIPad.git")
 else:
- porcelain.clone("https://github.com/Enviction/LiberIPad.git", ".")
+ porcelain.clone("https://github.com/E3nviction/LiberIPad.git", ".")
